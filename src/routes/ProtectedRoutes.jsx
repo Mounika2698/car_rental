c3e88267c346b0ed7c8ada98a74b9006e46e7441
Mounika Mani <mounikamani2698@gmail.com>
2026-01-12 20:20:57 -0600

    Worked on folder structure and routes

---
+++ src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ Children }) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+    return Children
+}
+
+export default ProtectedRoutes;
