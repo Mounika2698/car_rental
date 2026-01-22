781915d12494dece115f547121cc6699b5167a4c
prakathi-001 <prakathipragas@gmail.com>
2026-01-21 21:16:40 -0600

    5000 to 5001

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
     res.send("API running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
